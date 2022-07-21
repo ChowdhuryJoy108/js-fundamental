ef7108e1074c5eaa4f6b46d2c96e38674faf2262
Joy Chowdhury <imjoychy69@gmail.com>
2022-07-21 19:21:52 +0600

    while/do-while loop

---
+++ while.js
@@ -0,0 +1,24 @@
+var num = 15;
+while(num > 0){
+    console.log(num);
+    num--;
+}
+
+console.log("End of 1st while loop");
+
+
+var num2 = 0;
+while(num2 <= 15){
+    console.log(num2);
+    num2++;
+}
+
+//do-while loop js
+
+var num3 = 0;
+
+do{
+    console.log(num3);
+    num3++;
+
+}while(num3 <= 15)
