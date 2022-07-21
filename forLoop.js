aceba4107889d23581803691c1b95e2576a2f5fd
Joy Chowdhury <imjoychy69@gmail.com>
2022-07-21 19:54:10 +0600

    structure of for loop/ apply for loop to accessing elements of an  array

---
+++ forLoop.js
@@ -0,0 +1,27 @@
+
+
+// structure of for loop 
+// for(var i = 0; i <= 10; i++){
+//     console.log(i);
+// }
+
+
+// use of for in an array.
+
+var nums= [111,222,333,444,555,666,777,888];
+
+for (var i = 0; i < nums.length; i++){
+    var element = nums[i];
+    console.log(element);
+}
+
+// var nums2 = [55,66,77,88,99,199];
+
+// var element = [];
+
+// for(var i = 0; i<nums2.length; i++){
+//     element.push(nums2[i]);
+// }
+
+// console.log(element);
+
